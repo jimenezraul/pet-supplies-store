3a80908a511c3214680bd7d330aa5b18e449c767
Raul <jimenezraul1981@gmail.com>
2022-05-23 22:18:40 -0400

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className='bg-blue-50 min-h-screen flex flex-col justify-between'>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-1">
+          <h1>Home</h1>
     </div>
   );
 };
