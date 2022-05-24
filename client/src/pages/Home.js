a9f01adb640158242da774135a0dc49b8dc386a6
obrailovska <olabrrai@icloud.com>
2022-05-24 14:07:06 -0400

    added categories section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,11 @@ import imgCat from "../assets/homepage/cat.jpeg";
 import imgFish from "../assets/homepage/fish2.jpeg";
 import imgParrots from "../assets/homepage/parrots.jpeg";
 import imgDog from "../assets/homepage/dog.jpeg";
+import categoryDog from "../assets/categories/dogs/dog.jpeg";
+import categoryCat from "../assets/categories/cats/cat.jpeg";
+import categoryFish from "../assets/categories/fish/fish.jpeg";
+import categoryHamster from "../assets/categories/hamsters/hamster.jpeg";
+import categoryParrot from "../assets/categories/parrots/parrot.jpeg";
 
 const slideImages = [
   {
@@ -43,12 +48,59 @@ const Home = () => {
           ))}
         </Slide>
       </div>
-      <div className="midSection">
+      <div className="midSection ">
         <h1>Welcome to ProPet </h1>
         <button className="shopButton">Shop Now</button>
+      </div>
+      <div className="bg-white">
+        <div>
+          <p className="font-bold">Shop By Pet</p>
+        </div>
+        <div className="categories bg-white">
+          <div className="grid grid-cols-5 gap-4 justify-items-center">
+            <div>
+              <img alt="dog category" src={categoryDog} />
+            </div>
+            <div>
+              <img alt="cat category" src={categoryCat} />
+            </div>
+            <div>
+              <img alt="fish category" src={categoryFish} />
+            </div>
+            <div>
+              <img alt="hamster category" src={categoryHamster} />
+            </div>
+            <div>
+              <img alt="Parrot category" src={categoryParrot} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Home;
+
+/* <ul className="categoryList">
+          <li>
+            <img alt="dog category" src={categoryDog} />
+            <p>Dogs</p>
+          </li>
+          <li>
+            <img alt="cat category" src={categoryCat} />
+            <p>Cats</p>
+          </li>
+          <li>
+            <img alt="fish category" src={categoryFish} />
+            <p>Fish</p>
+          </li>
+          <li>
+            <img alt="hamster category" src={categoryHamster} />
+            <p>Hamsters</p>
+          </li>
+          <li>
+            <img alt="parrots category" src={categoryParrot} />
+            <p>Parrots</p>
+          </li>
+        </ul> */
