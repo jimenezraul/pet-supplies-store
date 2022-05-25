d26ecf2ba7d9cd822e84fbe35f017eafa2ad1690
Raul <jimenezraul1981@gmail.com>
2022-05-24 22:21:14 -0400

    add store with categories and products

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from './components/Footer';
+import Store from './pages/Store';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
         </Routes>
         <Footer />
       </div>
