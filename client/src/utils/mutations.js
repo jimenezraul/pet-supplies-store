b254ee58e72bbc120dbd58f63f00ca826878d0f2
Raul <jimenezraul1981@gmail.com>
2022-05-26 18:03:20 -0400

    add login mutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
