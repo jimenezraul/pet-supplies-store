f873eb0534a4763b6eda7f951e8af33de1ae3da5
obrailovska <olabrrai@icloud.com>
2022-05-26 20:12:03 -0400

    finished up log in and sign up forms

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { LOGIN } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
-  const logInHandler = (e) => {
-    e.preventDefault();
-    console.log(e.target);
+  const [formState, setFormState] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const [login, { error }] = useMutation(LOGIN);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
   };
   return (
     <section>
@@ -11,7 +38,7 @@ const LogIn = () => {
         <div className="xl:ml-20 xl:w-4/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0 ">
           <form
             className="bg-white p-10 border-solid border-2 border-grey-600 shadow-lg"
-            onSubmit={logInHandler}
+            onSubmit={handleFormSubmit}
           >
             <h1 className="font-semibold mb-8 text-3xl leading-snug uppercase text-center">
               Log In
@@ -21,33 +48,49 @@ const LogIn = () => {
                 type="text"
                 className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                 placeholder="Email address"
+                name="email"
+                onChange={handleChange}
               />
             </div>
 
             <div className="mb-6">
               <input
                 type="password"
+                name="password"
                 className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                 placeholder="Password"
+                onChange={handleChange}
               />
             </div>
 
             <div className="text-center lg:text-left">
+              {error && (
+                <div>
+                  <p className="error-text text-center text-red-700  pb-5">
+                    The provided credentials are incorrect
+                  </p>
+                </div>
+              )}
               <button
                 type="submit"
                 className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
               >
                 Login
-                <img id="buttonLogo" src="/assets/logo/logo.svg" className="w-full" alt="Logo" />
+                <img
+                  id="buttonLogo"
+                  src="/assets/logo/logo.svg"
+                  className="w-full"
+                  alt="Logo"
+                />
               </button>
               <p className="text-sm font-semibold mt-2 pt-1 mb-0">
                 Don't have an account?
-                <a
-                  href="/signup"
+                <Link
+                  to="/signup"
                   className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
                 >
                   Sign Up
-                </a>
+                </Link>
               </p>
             </div>
           </form>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
     <div className="bg-grey-lighter my-5 flex flex-col">
-      <div className="xl:ml-20 xl:w-4/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0 mx-auto">
+      <div className="xl:w-4/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0 mx-auto">
         <div className="bg-white px-6 py-8 rounded shadow-lg text-black w-full">
           <h1 className="font-semibold mb-8 text-3xl leading-snug uppercase text-center">
             Sign Up
@@ -41,19 +42,24 @@ const SignUp = () => {
               className="signUpBtn inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
             >
               Sign Up
-              <img id="signUpBtn" src="/assets/logo/logo.svg" className="w-full" alt="Logo" />
+              <img
+                id="signUpBtn"
+                src="/assets/logo/logo.svg"
+                className="w-full"
+                alt="Logo"
+              />
             </button>
           </form>
         </div>
 
         <div className="text-sm font-semibold mt-2 pt-1 mb-0">
           Already have an account?
-          <a
+          <Link
             className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
-            href="../login/"
+            to ="../login/"
           >
             Log in
-          </a>
+          </Link>
           .
         </div>
       </div>
