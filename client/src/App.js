e251f346bbc17f2285f6e31f247a8a5587fcc472
Chris Waters <floridarealty001@gmail.com>
2022-05-28 10:19:47 -0400

    loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+// import { render } from "react-dom";
+import Loading from "./components/Loading/index.js";
+
+
+
 function App() {
   return (
     <div className="App">
+    <Loading />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
