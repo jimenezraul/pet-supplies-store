c4a5b4b8d50045214730ac5bee8022aa4a6d0be9
Chris Waters <floridarealty001@gmail.com>
2022-06-01 15:01:36 -0400

    404 page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 // import { render } from "react-dom";
-import Loading from "./components/Loading/index.js";
+//import Loading from "./components/Loading/index.js";
+import PageNotFound from './components/404page/index.js';
+
 
 
 
 function App() {
   return (
     <div className="App">
-    <Loading />
+   
+      <PageNotFound />
     </div>
   );
 }
---
+++ client/src/components/404page/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './images/404image.jpg';
+
+
+const PageNotFound = () => (
+	
+	<div className="grid place-items-center h-screen">
+<img src={logo} alt="404 Page not Found"/>
+</div>
+	
+	
+
+  );
+
+  export default PageNotFound
