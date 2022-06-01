964cb58bfff15d40d24a8c6daf06be98943f9801
obrailovska <olabrrai@icloud.com>
2022-05-31 20:49:28 -0400

    signUp form works with back end

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -9,6 +9,7 @@ const ProductList = () => {
   const auth = Auth.loggedIn();
   const [add2Cart] = useMutation(ADD_TO_CART);
   const products = useSelector((state) => state.store.products);
+  console.log("HELOOO", products);
   const cart = useSelector((state) => state.store.cart);
   const dispatch = useDispatch();
 
@@ -50,31 +51,31 @@ const ProductList = () => {
     }
   };
   return (
-    <div className='w-full md:w-8/12 mb-5'>
-      <div className='bg-white border rounded shadow-lg flex flex-wrap py-5'>
+    <div className="w-full md:w-8/12 mb-5">
+      <div className="bg-white border rounded shadow-lg flex flex-wrap py-5">
         {products.map((product) => (
           <div
-            className='w-6/12 lg:w-4/12 flex justify-center p-2'
+            className="w-6/12 lg:w-4/12 flex justify-center p-2"
             key={product._id}
           >
-            <div className='max-w-sm rounded overflow-hidden'>
+            <div className="max-w-sm rounded overflow-hidden">
               <img
-                className='h-52 mx-auto'
+                className="h-52 mx-auto"
                 src={product.image_url}
                 alt={product.name}
               />
-              <div className='px-6 py-4'>
+              <div className="px-6 py-4">
                 <Link to={`/store/product/${product._id}`}>
-                  <div className='font-bold text-xl mb-2 hover:text-blue-700 hover:underline'>
+                  <div className="font-bold text-xl mb-2 hover:text-blue-700 hover:underline">
                     {product.name}
                   </div>
                 </Link>
-                <p className='text-gray-700 font-semibold'>${product.price}</p>
+                <p className="text-gray-700 font-semibold">${product.price}</p>
               </div>
-              <div className='px-6 pt-4 pb-2'>
+              <div className="px-6 pt-4 pb-2">
                 <button
                   onClick={() => addToCartHandler(product)}
-                  className='ml-1 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent'
+                  className="ml-1 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent"
                 >
                   Add To Cart
                 </button>
