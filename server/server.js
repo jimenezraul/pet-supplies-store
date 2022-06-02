cab72bf12ea2fdd1ff7922263feb9c6fab38341b
Raul <jimenezraul1981@gmail.com>
2022-06-02 16:41:36 -0400

    removed commented line in server js

--- server/server.js
+++ server/server.js
@@ -27,9 +27,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
