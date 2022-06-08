919621980df6ca4f0da5b60373fdaa39408d5d89
Raul <jimenezraul1981@gmail.com>
2022-06-08 11:20:47 -0400

    added service worker path to server

--- client/public/worker.js
+++ client/public/worker.js
@@ -38,7 +38,7 @@ self.addEventListener('fetch', event => {
 
 // Update a service worker
 self.addEventListener('activate', event => {
-  var cacheWhitelist = ['pwa-task-manager'];
+  var cacheWhitelist = ['pet-store'];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
--- server/server.js
+++ server/server.js
@@ -27,6 +27,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "../client/build/service-worker.js"));
+});
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
