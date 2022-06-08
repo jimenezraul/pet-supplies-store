e5fb5b38754eb2a640cbcd8d19638154430430fa
Raul <jimenezraul1981@gmail.com>
2022-06-08 10:57:32 -0400

    added pwa and removed unused files

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -19,3 +20,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
