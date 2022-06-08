01edae4c9a581b18699568a24c1cde977bf5d0a8
Raul <jimenezraul1981@gmail.com>
2022-06-08 11:52:25 -0400

    update service worker

--- server/server.js
+++ server/server.js
@@ -27,9 +27,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "../client/build/service-worker.js"));
+app.get("/worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "../client/build/worker.js"));
 });
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
