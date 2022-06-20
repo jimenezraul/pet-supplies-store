01ccad5492bf7a5d3d434ff2c1e9c510893e2356
Raul <jimenezraul1981@gmail.com>
2022-06-20 17:00:47 -0400

    removed margin y on 404 page

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const PageNotFound = () => (
   <section class='flex items-center h-full p-16'>
-    <div class='container flex flex-col items-center justify-center md:px-5 mx-auto my-8'>
+    <div class='container flex flex-col items-center justify-center md:px-5 mx-auto'>
       <div class='max-w-md text-center bg-white p-10 md:p-20 rounded-lg shadow-lg'>
         <h2 class='mb-8 font-extrabold text-9xl dark:text-gray-600'>
           <span class='sr-only'>Error</span>404
