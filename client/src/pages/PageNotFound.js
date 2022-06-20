8318983bbf74d0b343d67f667597c1919a7f4ed9
Raul <jimenezraul1981@gmail.com>
2022-06-20 16:38:01 -0400

    update 404 page not found

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,9 +1,16 @@
 import React from "react";
-import logo from "./images/404image.jpg";
+import "./PageNotFound.css";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => (
-  <div className='grid place-items-center h-screen'>
-    <img src={logo} alt='404 Page not Found' />
+  <div className='not-found text-center flex-1'>
+    <h1>404</h1>
+    <div className='cloak__wrapper'>
+      <div className='cloak__container'>
+        <div className='cloak'></div>
+      </div>
+    </div>
+    <h2 className='text-gray-400'>Page Not Found</h2>
   </div>
 );
 
