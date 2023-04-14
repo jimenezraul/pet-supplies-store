f584c4d039a1c7037df11ee7f3e83a47730c0de9
Raul <jimenezraul1981@gmail.com>
2023-04-13 23:20:54 -0400

    update vercel to deploy separately

--- server/server.js
+++ server/server.js
@@ -23,19 +23,19 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 // Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
 
-app.get("/service-worker.js", (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, "public", "../client/src/service-worker.js")
-  );
-});
+// app.get("/service-worker.js", (req, res) => {
+//   res.sendFile(
+//     path.resolve(__dirname, "public", "../client/src/service-worker.js")
+//   );
+// });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
